1c1235ce227edf83552a45c9066147ba70331461
Grim Maple <grimmaple95@gmail.com>
2019-07-13 17:10:08 +0300

    Splitting algorithm implemented

--- include/mpp/algorithm.hpp
+++ include/mpp/algorithm.hpp
@@ -13,6 +13,8 @@
 #ifndef _MPP_ALGORITHM_
 #define _MPP_ALGORITHM_
 
+#include <vector>
+
 namespace mpp
 {
 
@@ -20,6 +22,82 @@ template<typename F, typename T>
 T by_bit_cast(F from)
 {
     return *reinterpret_cast<T*>(&from);
+}
+
+template<class It, class T>
+std::vector<std::vector<T>> split(It begin, It end, const T& ref)
+{
+    while(*begin == ref) // Skip all possible occurances of split element in the beginning
+        begin++;
+
+    std::vector<std::vector<T>> ret;
+
+    auto add = [] (It& b, It& e)
+    {
+        std::vector<T> a;
+        if(e - b > 0)
+            a = std::vector<T>(b, e);
+
+        return a;
+    };
+
+    auto cur = begin;
+    auto last = begin;
+    while((++cur) != end)
+    {
+        if(*cur != ref)
+            continue;
+
+        auto added = add(last, cur);
+        if(added.size() != 0)
+            ret.push_back(added);
+        last = cur;
+        last++; // Makes it point to a non splitting character
+    }
+
+    auto added = add(last, cur);
+    if(added.size() != 0)
+        ret.push_back(added);
+
+    return ret;
+}
+
+std::vector<std::string> split(std::string::iterator begin, std::string::iterator end, const char ref)
+{
+    while(*begin == ref) // Skip all possible occurances of split element in the beginning
+        begin++;
+
+    std::vector<std::string> ret;
+
+    auto add = [] (std::string::iterator& b, std::string::iterator& e)
+    {
+        std::string a;
+        if(e - b > 0)
+            a = std::string(b, e);
+
+        return a;
+    };
+
+    auto cur = begin;
+    auto last = begin;
+    while((++cur) != end)
+    {
+        if(*cur != ref)
+            continue;
+
+        auto added = add(last, cur);
+        if(added.size() != 0)
+            ret.push_back(added);
+
+        last = cur;
+        last++; // Makes it point to a non splitting character
+    }
+
+    auto added = add(last, cur);
+    if(added.size() != 0)
+        ret.push_back(added);
+
+    return ret;
 }
 
 }
--- tests/algorithm.cxx
+++ tests/algorithm.cxx
@@ -9,6 +9,49 @@ TEST(algorithm, bit_cast)
     ASSERT_EQ(float1, res);
 }
 
+TEST(algorithm, split)
+{
+    std::vector<int> check = {1,2,3,0,4,5,6,0,7,8,9};
+    auto res = mpp::split(check.begin(), check.end(), 0);
+    ASSERT_EQ(res.size(), 3);
+
+    ASSERT_EQ(res[0].size(), 3);
+    ASSERT_EQ(res[0][0], 1);
+    ASSERT_EQ(res[0][1], 2);
+    ASSERT_EQ(res[0][2], 3);
+
+    ASSERT_EQ(res[1].size(), 3);
+    ASSERT_EQ(res[1][0], 4);
+    ASSERT_EQ(res[1][1], 5);
+    ASSERT_EQ(res[1][2], 6);
+
+    ASSERT_EQ(res[2].size(), 3);
+    ASSERT_EQ(res[2][0], 7);
+    ASSERT_EQ(res[2][1], 8);
+    ASSERT_EQ(res[2][2], 9);
+
+    std::vector<int> check2 = {0,0, 1,2,3,0,0, 3, 0};
+    res = mpp::split(check2.begin(), check2.end(), 0);
+    ASSERT_EQ(res.size(), 2);
+    ASSERT_EQ(res[0].size(), 3);
+    ASSERT_EQ(res[0][0], 1);
+    ASSERT_EQ(res[0][1], 2);
+    ASSERT_EQ(res[0][2], 3);
+    ASSERT_EQ(res[1].size(), 1);
+    ASSERT_EQ(res[1][0], 3);
+}
+
+TEST(algorithm, split_string)
+{
+    std::string check = "Hello, world, you, suck";
+    auto res = mpp::split(check.begin(), check.end(), ',');
+    ASSERT_EQ(res.size(), 4);
+    ASSERT_STREQ(res[0].c_str(), "Hello");
+    ASSERT_STREQ(res[1].c_str(), " world");
+    ASSERT_STREQ(res[2].c_str(), " you");
+    ASSERT_STREQ(res[3].c_str(), " suck");
+}
+
 int main(int argc, char *argv[])
 {
     testing::InitGoogleTest(&argc, argv);
